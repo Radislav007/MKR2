314c7c461599d1f562446079e799081ba951f430
Radislav007 <tevezradislav7@gmail.com>
2025-05-23 15:21:19 +0300

    Merged local

--- mkr/gallery/tests.py
+++ mkr/gallery/tests.py
@@ -4,49 +4,34 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from datetime import date
 
 class CategoryModelTest(TestCase):
-    def test_category_str_and_creation(self):
-        """Тестуємо створення категорії та метод __str__"""
+    def test_category_creation(self):
         category = Category.objects.create(name="Природа")
         self.assertEqual(str(category), "Природа")
         self.assertEqual(Category.objects.count(), 1)
-        self.assertEqual(Category.objects.first().name, "Природа")
-
 
 class ImageModelTest(TestCase):
     def setUp(self):
-        """Створення початкових даних перед кожним тестом"""
-        self.category_sea = Category.objects.create(name="Море")
-        self.category_mountains = Category.objects.create(name="Гори")
+        self.category1 = Category.objects.create(name="Море")
+        self.category2 = Category.objects.create(name="Гори")
 
-    def create_test_image(self, title="Відпочинок"):
-        """Допоміжний метод для створення тестового зображення"""
-        test_image_file = SimpleUploadedFile(
+    def test_image_creation_with_categories(self):
+        # Створюємо фейковий файл зображення
+        image_file = SimpleUploadedFile(
             name='test_image.jpg',
-            content=b'\x47\x49\x46\x38\x39\x61',  # GIF header, допустимий як fake image
+            content=b'\x47\x49\x46\x38\x39\x61',
             content_type='image/jpeg'
         )
-        return Image.objects.create(
-            title=title,
-            image=test_image_file,
+
+        image = Image.objects.create(
+            title="Відпочинок",
+            image=image_file,
             created_date=date.today(),
             age_limit=12
         )
-
-    def test_image_creation_and_category_assignment(self):
-        """Тестуємо створення зображення і призначення категорій"""
-        image = self.create_test_image()
-
-        image.categories.set([self.category_sea, self.category_mountains])
-        image.save()
+        image.categories.set([self.category1, self.category2])
 
         self.assertEqual(str(image), "Відпочинок")
         self.assertEqual(image.categories.count(), 2)
         self.assertEqual(Image.objects.count(), 1)
-        self.assertIn(self.category_sea, image.categories.all())
-        self.assertIn(self.category_mountains, image.categories.all())
-
-    def test_image_without_categories(self):
-        """Тестуємо створення зображення без категорій"""
-        image = self.create_test_image(title="Без категорій")
-        self.assertEqual(image.categories.count(), 0)
-        self.assertEqual(Image.objects.count(), 1)
+        self.assertIn(self.category1, image.categories.all())
+        self.assertIn(self.category2, image.categories.all())
